b4d19cfa76415036104d85795cf0bea8dd33a4ae
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-07-13 23:44:51 -0400

    Created the WordLevel script
    Next step: Laying Out the Screen

--- Assets/__Scripts/WordGame.cs
+++ Assets/__Scripts/WordGame.cs
@@ -16,6 +16,8 @@ public class WordGame : MonoBehaviour {
 
     [Header("Set Dynamically")]
     public GameMode mode = GameMode.preGame;
+    public WordLevel currLevel;
+
     void Awake() {
         S = this;
     }
@@ -26,8 +28,59 @@ public class WordGame : MonoBehaviour {
         WordList.INIT();
     }
 
-    public void WordListParseComplete()
-    {
+    public void WordListParseComplete() {
         mode = GameMode.makeLevel;
+
+        currLevel = MakeWordLevel();
+    }
+
+    public WordLevel MakeWordLevel(int levelNum = -1) {
+        WordLevel level = new WordLevel();
+        if (levelNum == -1) {
+
+            level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
+        } else {
+            // This will be added later in the chapter
+        }
+        level.levelNum = levelNum;
+        level.word = WordList.GET_LONG_WORD(level.longWordIndex);
+        level.charDict = WordLevel.MakeCharDict(level.word);
+
+        StartCoroutine(FindSubWordsCoroutine(level));
+        return (level);
+    }
+
+    public IEnumerator FindSubWordsCoroutine(WordLevel level) {
+        level.subWords = new List<string>();
+        string str;
+
+        List<string> words = WordList.GET_WORDS();
+
+        for (int i = 0; i < WordList.WORD_COUNT; i++) {
+            str = words[i];
+
+            if (WordLevel.CheckWordInLevel(str, level)) {
+                level.subWords.Add(str);
+            }
+
+            if (i % WordList.NUM_TO_PARSE_BEFORE_YIELD == 0) {
+
+                yield return null;
+            }
+        }
+
+        level.subWords.Sort();
+        level.subWords = SortWordsByLength(level.subWords).ToList();
+
+        SubWordSearchComplete();
+    }
+
+    public static IEnumerable<string> SortWordsByLength(IEnumerable<string> ws) {
+        ws = ws.OrderBy(s => s.Length);
+        return ws;
+    }
+
+    public void SubWordSearchComplete() {
+        mode = GameMode.levelPrep;
     }
 }
